4e70fb080c42175f880b5e24cfa9ceffea96ac1f
Syrym Zhursin <jurssin@gmail.com>
2020-09-18 16:32:17 +0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by User on 9/18/20.
+//  Copyright © 2020 Syrym Zhursin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
